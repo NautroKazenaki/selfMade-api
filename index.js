71768da9f28cb1d2537bae341a4fc95558fc015c
Сергей Ашарин <kalliak2014@yandex.ru>
2022-01-29 17:27:52 +0300

    1st api call success

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = '26f92a4ed9d0e7d23705e9c2c3bf0ecf'
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
 //command to parse json
 app.use(express.json());
 
@@ -11,5 +14,20 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Welcome to my first api')
 });
+
+//get product details
+app.get('/products/:productId', async (req, res) => {
+    const {productId} = req.params
+
+    try {
+        //data about specific product
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(response);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 // project start
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
